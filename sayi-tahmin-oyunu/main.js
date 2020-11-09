16739d3e020c08a3f0ae684b3bf0a200ee1e9fd8
Abdullah ARIK <mail@abdullaharik.com>
2020-11-09 20:26:09 +0300

    geliştirmeler yapıldı

--- sayi-tahmin-oyunu/main.js
+++ sayi-tahmin-oyunu/main.js
@@ -1,7 +1,13 @@
+/**
+ * Abdullah Arik
+ * @abdullaharik
+ * 8 Kasım 2020
+ *
+ * Erdem'le birlikte
+ */
 
-
-const enAz = 1
-const enFazla = 20
+const enAz = 50
+const enFazla = 100
 
 /**
  * 0 * 20 + 1 = 1
@@ -9,25 +15,48 @@ const enFazla = 20
  * 0.7 * 14 + 1 = 15
  * 0.7204738363529 * 14 + 1 = 15.2385504736 > 15
  */
-const sayi = Math.floor(Math.random() * 20 + 1)
+const sayi = Math.floor(Math.random() * enFazla + enAz)
 
+const aciklama = document.getElementById('aciklama').innerHTML = `${enAz} ile ${enFazla} arasındaki sayıyı tahmin edin.`
+
+const tahmin = document.getElementById('tahmin')
 const tahminler = document.getElementById('tahminler')
 
-const hesapla = function () {
-  const tahmin = document.getElementById('tahmin').value
+const tahminEt = document.getElementById('tahminEt').addEventListener('click', hesapla)
 
-  if (sayi == tahmin) {
-    tahminler.innerHTML += `<li>${tahmin} : Bildiniz</li>`
+
+
+function hesapla(e) {
+  e.preventDefault()
+
+  /**
+   * TRUE : FALSE
+   *
+   * FALSE olan değerler
+   * 0 - Sıfır
+   * "" - Boş yazı değeri
+   *
+   * TRUE
+   * 1 ve daha büyük sayılar
+   * boş olmayan bütün yazılar
+   */
+
+  if (! tahmin.value) {
+    alert('Lütfen bir sayı yazınız.')
+    return
   }
 
-  if ( sayi > tahmin) {
-    tahminler.innerHTML += `<li>${tahmin} : Daha büyük bir sayı</li>`
+  if (sayi == tahmin.value) {
+    tahminler.innerHTML += `<li>${tahmin.value} : Bildiniz</li>`
   }
 
-  if ( sayi < tahmin) {
-    tahminler.innerHTML += `<li>${tahmin} : Daha küçük bir sayı</li>`
+  if ( sayi > tahmin.value) {
+    tahminler.innerHTML += `<li>${tahmin.value} : Daha büyük bir sayı</li>`
+  }
+
+  if ( sayi < tahmin.value) {
+    tahminler.innerHTML += `<li>${tahmin.value} : Daha küçük bir sayı</li>`
   }
 }
 
-const tahminEt = document.getElementById('tahminEt').addEventListener('click', hesapla)
 
